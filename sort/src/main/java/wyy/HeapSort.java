cba314c1bbb06dd0d92f43311560b0b42065b744
Feng <fengyibin713@gmail.com>
2015-06-17 16:59:26 +0800

    sorting

---
+++ sort/src/main/java/wyy/HeapSort.java
@@ -0,0 +1,17 @@
+package wyy;
+
+public class HeapSort <T extends Comparable<T>> {
+	private T[] array;
+
+	public T[] getArray() {
+		return array;
+	}
+
+	public void setArray(T[] array) {
+		this.array = array;
+	}
+	
+	public void sort(){
+		//
+	}
+}
---
+++ sort/src/test/java/wyy/HeapSortTest.java
@@ -0,0 +1,24 @@
+package wyy;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HeapSortTest {
+
+	@Test
+	public void testHeapSort(){
+		Integer[] data = {3,2,1,4,5,9,6,7,8,0};
+		HeapSort<Integer> heapSort = new HeapSort<Integer>();
+		heapSort.setArray(data);
+		heapSort.sort();
+		for (Integer integer : data) {
+			System.out.print(integer + " ");
+		}
+		
+		for (int i = 0; i < data.length; i++) {
+			assertEquals(data[i].intValue(), i);			
+		}
+	}
+
+}
