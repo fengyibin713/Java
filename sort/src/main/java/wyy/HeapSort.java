b7a0ebfd9fe145f66acb9cda6c54e1fb9e366e4a
wuyunyi <muqiwyy@163.com>
2015-06-19 23:28:01 +0800

    getChild

--- sort/src/main/java/wyy/HeapSort.java
+++ sort/src/main/java/wyy/HeapSort.java
@@ -12,16 +12,28 @@ public class HeapSort <T extends Comparable<T>> {
 	}
 	
 	public void sort(){
-		for (int i = 1; i < array.length; i++) {
-			int y = i;
-			while ((y > 0) && (array[y].compareTo(array[y - 1]) < 0)) {
-				T c;
-				c = array[y];
-				array[y] = array[y - 1];
-				array[y - 1] = c;
-				y--;
-			}
-		}
+//		for (int i = 1; i < array.length; i++) {
+//			int y = i;
+//			while ((y > 0) && (array[y].compareTo(array[y - 1]) < 0)) {
+//				T c;
+//				c = array[y];
+//				array[y] = array[y - 1];
+//				array[y - 1] = c;
+//				y--;
+//			}
+//		}
 
+	}
+	
+	public T getLeftChild(int current) {
+		return array[current*2+1];
+	}
+	
+	public T getRightChild(int current){
+		return array[current*2+2];
+	}
+	
+	private static void compareWithChild(T[] array,int heapSize,int index){
+		//当前点与左右子节点比较
 	}
 }
